ad977c05299b6cabd750168ef16b4f64c6061ec0
Chris <chris.dance@papercut.com>
2012-10-24 11:44:16 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,90 @@
+Are You Sure?  - A "dirty forms" JQuery Plugin for modern browsers!
+======
+
+Are-you-sure (```jquery.are-you-sure.js```) is simple light-weight "dirty 
+forms" JQuery Plugin for modern browsers.  It helps prevent users from loosing 
+unsaved form changes.
+
+It's simple to use.  Just add the following line to your page's jquery 
+ready function:
+
+```javascript
+$('form').areYouSure();
+```
+
+Are-you-sure is a minimal plugin for modern browsers.  There of plenty of 
+"dirty forms" implementations out there, but their code is hairy! They often 
+have many hacks to support legacy browsers, and/or rely on other fat 
+dependencies such as FaceBox or jQueryUI.  
+
+Are-you-sure is as simple as it gets:
+
+ * 100% JS with zero dependencies or no external CSS.
+ * Leverage `onBeforeUnload` to detect all exit page/browser events.
+ * Works on forms of any size.
+ * Correct state management - if a user edits then restores a value, the form 
+   is not considered dirty.
+ * Easy to understand - less than a "terminal screen" of code!
+
+##Usage
+
+```javascript
+
+$(function() {
+
+    // Enable on all forms
+    $('form').areYouSure();
+
+    // Enable on selected forms
+    $('form.dirtyCheck').areYouSure();
+
+    // With a custom message
+    $('form').areYouSure( {'message':'Your profile details are not saved!'} );
+
+    // Test if a form is dirty in your own code
+    if ($('#myForm').hasClass('dirty')) {
+        // Do something
+    }
+}
+```
+To ignore selected fields from the dirtyness check: 
+
+```html
+    <form id="myForm" name="myform" action="/post" method="post">
+
+        Text:                           <input type="text" name="text1"> <br />
+        Text (ignored with attribute):  <input type="text" name="text2" data-ays-ignore="true"> <br />
+        Text (ignored with class):      <input type="text" name="text2" class="aysIgnore"> <br />
+
+        <input type="submit" value="Submit">
+
+	</form>
+```
+
+
+##Install
+This plugin is very light weight. You could download the 
+```jquery.are-you-sure.js``` however my recommendation is to simply 
+cut-n-paste the code into one of your existing JavaScript files... it seems 
+a shame to add an extra browser round trip for such a simple feature :-)
+
+
+##Release History
+
+**2012-10-24** - Initial public release.
+
+
+##Prerequisites
+jQuery version 1.4.2 or higher. 
+
+
+##License
+The same as JQuery...
+
+    jQuery Plugin: Are-You-Sure
+    https://github.com/codedance/jquery.AreYouSure/
+ 
+    Copyright (c) 2012, Chris Dance - PaperCut Software http://www.papercut.com/
+    Dual licensed under the MIT or GPL Version 2 licenses.
+    http://jquery.org/license
+
---
+++ jquery.are-you-sure.js
@@ -0,0 +1,104 @@
+/*!
+ * jQuery Plugin: Are-You-Sure
+ * https://github.com/codedance/jquery.AreYouSure/
+ *
+ * Copyright (c) 2012, Chris Dance - PaperCut Software http://www.papercut.com/
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Date: 24th Oct 2012
+ */
+(function($) {
+  $.fn.areYouSure = function(options) {
+
+    var settings = $
+        .extend(
+            {
+              'message' : 'You have unsaved changes!',
+              'dirtyClass' : 'dirty',
+              'fieldSelector' : "select,textarea,input[type='text'],input[type='password'],input[type='checkbox'],input[type='radio']"
+            }, options);
+
+    var getValue = function($field) {
+      if ($field.hasClass('aysIgnore') || $field.attr('data-ays-ignore')) {
+        return null;
+      }
+
+      var val;
+      var type = $field.attr('type');
+      if ($field.is('select'))
+        type = 'select';
+
+      switch (type) {
+      case 'checkbox':
+      case 'radio':
+        val = $field.is(':checked');
+        break;
+      case 'select':
+        val = '';
+        $field.children('option').each(function(o) {
+          var $option = $(this);
+          if ($option.is(':selected')) {
+            val += $option.val();
+          }
+        });
+        break;
+      default:
+        val = $field.val();
+      }
+      return val;
+    };
+
+    var storeOrigValue = function() {
+      var $field = $(this);
+      $field.data('ays-orig', getValue($field));
+    };
+
+    var checkForm = function(evt) {
+      var isFieldDirty = function($field) {
+        return (getValue($field) != $field.data('ays-orig'));
+      };
+
+      var isDirty = false;
+      var $form = $(this).parents('form');
+
+      // Test on the target first as it's the most likely to be dirty.
+      if (isFieldDirty($(evt.target))) {
+        isDirty = true;
+      }
+
+      if (!isDirty) {
+        $form.find(settings.fieldSelector).each(function() {
+          $field = $(this);
+          if (isFieldDirty($field)) {
+            isDirty = true;
+            return false; // break
+          }
+        });
+      }
+
+      $form.toggleClass(settings.dirtyClass, isDirty);
+    };
+
+    $(window).bind('beforeunload', function() {
+      var n = $("form[class='" + settings.dirtyClass + "']").length;
+      if (n > 0) {
+        return settings.message;
+      }
+    });
+
+    return this.each(function(elem) {
+      if (!$(this).is('form'))
+        return;
+
+      $(this).submit(function() {
+        $form.removeClass(settings.dirtyClass);
+      });
+
+      $(this).find(settings.fieldSelector).each(storeOrigValue);
+
+      //$(this).on('change keyup', settings.fieldSelector, checkForm);
+      $(this).find(settings.fieldSelector).bind('change keyup', checkForm);
+    });
+  };
+})(jQuery);
