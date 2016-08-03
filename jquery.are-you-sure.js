21a0010d75e31ffcf97eb58370dc2e16763b7bcb
Raymond Schweers <raymond@hetwebbureau.nl>
2016-08-03 15:39:25 +0200

    fix for not defined var

--- jquery.are-you-sure.js
+++ jquery.are-you-sure.js
@@ -88,7 +88,7 @@
         return;
       }
 
-      $fields = $form.find(settings.fieldSelector);
+      var $fields = $form.find(settings.fieldSelector);
 
       if (settings.addRemoveFieldsMarksDirty) {              
         // Check if field count has changed
@@ -102,7 +102,7 @@
       // Brute force - check each field
       var isDirty = false;
       $fields.each(function() {
-        $field = $(this);
+        var $field = $(this);
         if (isFieldDirty($field)) {
           isDirty = true;
           return false; // break
